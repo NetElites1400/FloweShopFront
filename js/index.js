2e5a506f406bbd633b298ae679dcfc6f2e588fe5
mortezash <mortazesh1380@gmail.com>
2022-04-28 23:41:02 +0430

    start_project

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const address = document.getElementById("address");
+const address2 = document.getElementById("address2");
+const address3 = document.getElementById("address3");
+const address4 = document.getElementById("address4");
+
+address.addEventListener("click", background);
+
+function background() {
+    address.classList.add("address");
+}
+
+address2.addEventListener("click", background);
+
+function background() {
+    address2.classList.add("address");
+}
+
+address3.addEventListener("click", background);
+
+function background() {
+    address3.classList.add("address");
+}
+
+address4.addEventListener("click", background);
+
+function background() {
+    address4.classList.add("address");
+}
